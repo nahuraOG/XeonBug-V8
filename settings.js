ec9dc51580c1712a244bda0e237a96bb8697e44c
nahuraOG <ilovenga@hotmail.com>
2024-10-19 17:48:10 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,15 +10,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
-global.ytname = "YT: GlobalTechInfo"
-global.socialm = "GitHub: GlobalTechInfo"
-global.location = "Pakistan, Karachi, Karachi"
+global.ownernomer = "4917720256629"
+global.ownername = "Nahura"
+global.ytname = ""
+global.socialm = "GitHub: nahuraOG"
+global.location = "Germany, Esex, Esex"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
-global.botname = 'XeonBug-V8' //name of the bot
+global.ownernumber = '4917820256629'  //creator number
+global.ownername = 'Nahura' //owner name
+global.botname = 'Nahura Bot-V1' //name of the bot
 
 //sticker details
 global.packname = 'Sticker By'
